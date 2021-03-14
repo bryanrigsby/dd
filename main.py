27794432a7600f50f316902cb876f01163f3abb0
Dylan <drpheonixgaming2288@gmail.com>
2021-03-14 17:01:22 -0500

    Added more stuff
    Added commands and a lot of stuff.

--- main.py
+++ main.py
@@ -4,20 +4,29 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+import webbrowser
+import os
+import json
+
+with open('config.json') as c:
+    data = json.load(c)
+
+nowtime = datetime.datetime.now()
 
 datetime_now = datetime.datetime.now()
-test = datetime.datetime(datetime_now.year, datetime_now.month, datetime_now.day, 17, 00, 1)
-midnight = datetime.datetime(datetime_now.year, datetime_now.month, datetime_now.day, 0, 00, 1)
-noon = datetime.datetime(datetime_now.year, datetime_now.month, datetime_now.day, 12, 00, 00)
-fivePM = datetime.datetime(datetime_now.year, datetime_now.month, datetime_now.day, 17, 00, 00)
-tod_responses = ['Good Morning, Sir', 'Good Afternoon, Sir', 'Good Evening, Sir']
+# test = datetime.datetime(datetime_now.year, datetime_now.month, datetime_now.day, 00, 00, 1)
+# midnight = datetime.datetime(datetime_now.year, datetime_now.month, datetime_now.day, 0, 00, 1)
+# noon = datetime.datetime(datetime_now.year, datetime_now.month, datetime_now.day, 12, 00, 00)
+# fivePM = datetime.datetime(datetime_now.year, datetime_now.month, datetime_now.day, 17, 00, 00)
+# tod_responses = ['Good Morning, Sir', 'Good Afternoon, Sir', 'Good Evening, Sir']
 
-listener = sr.Recognizer()
 engine = pyttsx3.init()
+listener = sr.Recognizer()
 voices = engine.getProperty('voices')
-engine.setProperty('voice', 'com.apple.speech.synthesis.voice.rishi')
+engine.setProperty('voices', 'com.apple.speech.synthesis.voice.rishi')
 
-## get list of voices
+
+# get list of voices
 # for voice in voices:
 #     print("Voice:")
 #     print(" - ID: %s" % voice.id)
@@ -33,6 +42,23 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+
+if nowtime.hour >= 19:
+    greeting = "Good evening Sir"
+    talk("Good evening Sir")
+
+elif nowtime.hour >= 12:
+    greeting = "Good afternoon sir"
+    talk("Good afternoon sir")
+
+elif nowtime.hour >= 00:
+    greeting = "Good morning sir"
+    talk("Good morning sir")
+
+# noinspection PyUnboundLocalVariable
+print("{}!".format(greeting))
+
+
 def take_command():
     try:
         with sr.Microphone() as source:
@@ -46,12 +72,11 @@ def take_command():
 
 
 
-
+# noinspection PyUnresolvedReferences
 def run_program():
     command = take_command()
     if 'dd' in command:
         command = command.replace('dd', '')
-
 
     elif 'play' in command:
         song = command.replace('play', '')
@@ -59,9 +84,10 @@ def run_program():
         pywhatkit.playonyt(song)
 
     elif 'time' in command:
-        time = datetime.datetime.now().strftime('%H:%M')
-        talk('the time is ' + time)
-        print(time)
+        CurrentTime = datetime.datetime.strftime("%H:%M")
+        print("The time is ", CurrentTime)
+        talk("The time is", CurrentTime)
+        print(data)
 
     elif 'search' in command:
         person = command.replace('search', '')
@@ -76,13 +102,45 @@ def run_program():
         talk('Peace out')
         exit()
 
-    elif 'do' in command:
-        talk('I can tell time, search for info, play YouTube videos, and tell jokes')
+    elif utterances in command:
+        talk('I can tell time, search for info, play YouTube videos, tell jokes, and open applications.')
+
+    elif 'google' in command:
+        webbrowser.open('http://google.com', new=2)
+        talk('Opening google')
+
+    elif 'block game' or 'minecraft' in command:
+        os.system('open -a Minecraft')
+        talk('Opening Minecraft')
+
+    elif 'roblox' in command:
+        os.system('open -a Roblox')
+        talk('Opening Roblox')
+
+    elif 'obs' or 'screen recorder' in command:
+        os.system('open -a OBS')
+        talk('Opening OBS')
+
+    elif 'spotify' or 'music' in command:
+        os.system('open -a Spotify')
+        talk('Opening Spotify')
+
+    elif 'lunar client' or 'client' in command:
+        os.system('open -a LunarClient')
+        talk('Opening Lunar Client')
+
+    elif 'discord' or 'communication' in command:
+        os.system('open -a Discord')
+        talk('Opening Discord')
+
+    elif 'slack' in command:
+        os.system('open -a Slack')
+        talk('Opening Slack')
+
+
 
     else:
         talk('I do not understand')
-
-
 
 
 while True:
